965c55fad137f0ce2c9bd07a5b9ca528b0430fea
Your NameChristian <christianhermanndev@gmail.com>
2026-03-08 11:15:19 -0500

    Adding animal routes

--- Routes.js
+++ Routes.js
@@ -88,3 +88,47 @@ app.delete("/expressions/:id", (req, res, next) => {
     res.status(404).send();
   }
 });
+
+// ADDING ANIMAL ROUTES
+app.get("/animals", (req, res, next) => {
+  res.send(animals);
+});
+
+app.get("/animals/:id", (req, res, next) => {
+  const foundAnimal = getElementById(req.params.id, animals);
+  if (foundAnimal) {
+    res.send(foundAnimal);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.put("/animals/:id", (req, res, next) => {
+  const animalIndex = getIndexById(req.params.id, animals);
+  if (animalIndex !== -1) {
+    updateElement(req.params.id, req.query, animals);
+    res.send(animals[animalIndex]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.post("/animals", (req, res, next) => {
+  const receivedAnimal = createElement("animals", req.query);
+  if (receivedAnimal) {
+    animals.push(receivedAnimal);
+    res.status(201).send(receivedAnimal);
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.delete("/animals/:id", (req, res, next) => {
+  const animalIndex = getIndexById(req.params.id, animals);
+  if (animalIndex !== -1) {
+    animals.splice(animalIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
