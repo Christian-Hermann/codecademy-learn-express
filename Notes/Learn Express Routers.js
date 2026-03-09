b79e4f7fd9d295fa06fb3ae981acebd79054750f
Your NameChristian <christianhermanndev@gmail.com>
2026-03-09 14:19:17 -0500

    Using multiple router files

--- Notes/Learn Express Routers.js
+++ Notes/Learn Express Routers.js
@@ -1,5 +1,21 @@
 // Express.Router
+const expressionsRouter = express.Router();
+app.use("/expressions", expressionsRouter);
 
 expressionsRouter.get("/", (req, res, next) => {
   res.send(expressions);
 });
+
+// USING MULTIPLE ROUTER FILES
+const express = require("express");
+const { seedElements } = require("./utils");
+const expressionsRouter = express.Router();
+
+const expressions = [];
+seedElements(expressions, "expressions");
+
+expressionsRouter.get("/", (req, res, next) => {
+  res.send(expressions);
+});
+
+module.exports = expressionsRouter;
