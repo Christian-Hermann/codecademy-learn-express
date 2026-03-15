d9756c1c0574a74fae52a8b9ff541fb8d6705797
Your NameChristian <christianhermanndev@gmail.com>
2026-03-15 11:20:33 -0500

    DRY with app.use()

--- Notes/Middleware.js
+++ Notes/Middleware.js
@@ -2,3 +2,8 @@
 const logRequest = (verb) => {
   console.log(`${verb} Request Received`);
 };
+
+// DRYING ROUTES WITH app.use()
+app.use((req, res, next) => {
+  console.log(`${req.method} Request Received`);
+});
