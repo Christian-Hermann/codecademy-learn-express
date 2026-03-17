d4ea85f434594d3842baa2862ef45369ef55f2be
Your NameChristian <christianhermanndev@gmail.com>
2026-03-17 12:11:30 -0500

    app.use() - multiple paths

--- Notes/Middleware.js
+++ Notes/Middleware.js
@@ -20,3 +20,18 @@ app.use("/beans/:beanName", (req, res, next) => {
   req.beanName = beanName;
   next();
 });
+
+// Route-Level app.use() - Multiple Paths
+// Add your code below:
+app.use(["/beans", "/beans/:beanName"], (req, res, next) => {
+  let bodyData = "";
+  req.on("data", (data) => {
+    bodyData += data;
+  });
+  req.on("end", () => {
+    if (bodyData) {
+      req.body = JSON.parse(bodyData);
+    }
+    next();
+  });
+});
