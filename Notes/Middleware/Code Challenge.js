e814d6872681b4e5ea1b3964157cbc006b561f96
Your NameChristian <christianhermanndev@gmail.com>
2026-03-30 14:57:57 -0500

    Code challenge setup

---
+++ Notes/Middleware/Code Challenge.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+// Finish this function and use it in the routes below
+const sendFruitResponse = (req, res, next) => {};
+
+app.get("/whatis/apple", (req, res, next) => {
+  res.send("fruit");
+});
+
+app.get("/whatis/orange", (req, res, next) => {
+  res.send("fruit");
+});
+
+app.get("/whatis/banana", (req, res, next) => {
+  res.send("fruit");
+});
+
+// export app for use in main.js and for testing
+module.exports = {
+  app,
+};
